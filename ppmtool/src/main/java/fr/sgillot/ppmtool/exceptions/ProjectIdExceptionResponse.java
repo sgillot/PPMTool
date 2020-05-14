1410367e1f0423312818027ce0cc8ef73912513b
s.gillot <s.gillot@waigeo.fr>
2020-05-14 15:36:52 +0200

    First final version

---
+++ ppmtool/src/main/java/fr/sgillot/ppmtool/exceptions/ProjectIdExceptionResponse.java
@@ -0,0 +1,4 @@
+package fr.sgillot.ppmtool.exceptions;
+
+public class ProjectIdExceptionResponse {
+}
---
+++ ppmtool/src/main/java/fr/sgillot/ppmtool/services/ValidationService.java
@@ -0,0 +1,12 @@
+package fr.sgillot.ppmtool.services;
+
+import org.springframework.http.ResponseEntity;
+
+import java.util.Map;
+
+public class ProjectErrorService {
+
+    public ResponseEntity<?>
+
+
+}
